e5f5c13b044e06f1bfea3052b44d0f291106bd76
Torbjörn Rathsman <milasudril@gmail.com>
2021-08-02 19:45:00 +0200

    More work on ColumnLayout

--- lib/column_layout.hpp
+++ lib/column_layout.hpp
@@ -1,26 +1,26 @@
 #ifndef FRUIT_COLUMN_LAYOUT_HPP
 #define FRUIT_COLUMN_LAYOUT_HPP
 
-#include "./event_dispatcher.hpp"
+#include "./event_handler.hpp"
 
 namespace fruit
 {
 	class ColumnLayout
 	{
 	public:
-		void push_back(LayoutBox box)
+		template<class LayoutBox>
+		void push_back(LayoutBox& object)
 		{
 			m_content.push_back(box);
 		}
 
 		void handle(ViewportSizeEvent const& event)
 		{
-			auto loc = m_location;
-
 		}
 
 	private:
-
+		std::vector<std::pair<EventHandler<SizeRequestEvent>,
+			EventHandler<SizeUpdateEvent>> m_content;
 	};
 }
 
