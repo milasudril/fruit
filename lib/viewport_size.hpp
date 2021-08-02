707493a61e0cd5c99d5691c45bf0007930c934fe
Torbjörn Rathsman <milasudril@gmail.com>
2021-08-02 19:46:30 +0200

    Add viewport size

---
+++ lib/viewport_size.hpp
@@ -0,0 +1,15 @@
+#ifndef FRUIT_VIEWPORTSIZE_HPP
+#define FRUIT_VIEWPORTSIZE_HPP
+
+#include "./event_handler.hpp"
+
+namespace fruit
+{
+	struct ViewportSize
+	{
+		int width;
+		int height;
+	};
+}
+
+#endif
