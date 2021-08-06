753186bc2934fff25863702089a7f47bc0e1e8d1
Torbjörn Rathsman <milasudril@gmail.com>
2021-08-06 18:13:42 +0200

    Add testcase to freetype fontface loader

---
+++ lib/freetype_fontface_loader.test.cpp
@@ -0,0 +1,16 @@
+//@	{"target":{"name":"freetype_fontface_loader.test"}}
+
+#include "./freetype_fontface_loader.hpp"
+
+#include "testfwk/testfwk.hpp"
+
+TESTCASE(FreetypeFontfaceLoaderInit)
+{
+	fruit::FreetypeFontfaceLoader loader;
+}
+
+TESTCASE(FreetypeFontfaceLoaderLoadFont)
+{
+	fruit::FreetypeFontfaceLoader loader;
+//	loader.load();
+}
