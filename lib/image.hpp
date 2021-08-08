530d50eff9766c6177401fbaac242089d09c97be
Torbjörn Rathsman <milasudril@gmail.com>
2021-08-08 10:04:04 +0200

    Add image class

---
+++ lib/image.hpp
@@ -0,0 +1,103 @@
+#ifndef FRUIT_IMAGE_HPP
+#define FRUIT_IMAGE_HPP
+
+#include "./image_view.hpp"
+
+namespace fruit
+{
+	template<class T>
+	class Image
+	{
+	public:
+		explicit Image(ImageView<T> view):Image{view.data(), view.width(), view.height()}{}
+
+		explicit Image(T* data, int width, int height):
+			m_data{std::make_unique<T[]>(width*height)},
+			m_width{width},
+			m_height{height}
+		{}
+
+		explicit Image(int width, int height):
+			m_data{std::make_unique<T[]>(width*height)},
+			m_width{width},
+			m_height{height}
+		{}
+
+		T& operator()(int x, int y)
+		{
+			return m_data[y*m_width + x];
+		}
+
+		T operator()(int x, int y) const
+		{
+			return m_data[y*m_width + x];
+		}
+
+		int width() const
+		{
+			return m_width;
+		}
+
+		int height() const
+		{
+			return m_height;
+		}
+
+		T const* data() const
+		{
+			return m_data.get();
+		}
+
+		T* data()
+		{
+			return m_data.get();
+		}
+
+		operator ImageView<T>()
+		{
+			return ImageView{data(), m_width, m_height};
+		}
+
+		operator ImageView<T const>() const
+		{
+			return ImageView{data(), m_width, m_height};
+		}
+
+	private:
+		std::unique_ptr<T[]> m_data;
+		int m_width;
+		int m_height;
+	};
+
+	template<class T>
+	size_t size(Image<T> img)
+	{
+		return img.width() * img.height();
+	}
+
+	template<class T>
+	T* data(Image<T> img)
+	{
+		return img.data();
+	}
+
+	template<class T>
+	T* begin(Image<T> img)
+	{
+		return img.data();
+	}
+
+	template<class T>
+	T* end(Image<T> img)
+	{
+		return img.data() + size(img);
+	}
+
+	template<class T>
+	std::span<T> make_span(Image<T> img)
+	{
+		return std::span{data(img), size(img)};
+	}
+}
+
+#endif
