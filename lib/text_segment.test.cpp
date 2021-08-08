0fd90f52bae8e3e0d065fc799b489f991ce6b454
Torbjörn Rathsman <milasudril@gmail.com>
2021-08-08 14:14:24 +0200

    Split testcase

--- lib/text_segment.test.cpp
+++ lib/text_segment.test.cpp
@@ -50,94 +50,127 @@ TESTCASE(TextSegmentScript)
 	EXPECT_EQ(static_cast<int>(segment.script()), static_cast<int>(fruit::WritingSystem::Arabic));
 }
 
-TESTCASE(TextSegmentShape)
+TESTCASE(TextSegmentShapeSwedish)
 {
 	fruit::FreetypeFontfaceLoader loader;
 	fruit::FreetypeFontFace face{loader, fruit::io_utils::load(fruit::FontMapper{}.get_path("DejaVu Sans"))};
 	fruit::TextShaper foobar{face};
 
 	fruit::TextSegment segment;
-	{
-		std::u8string_view const text{u8"Yxmördaren Julia Blomqvist på fäktning i Schweiz"};
-		auto shape_result = segment.text(text).language(fruit::LanguageTag{"sv-se"}).shape(foobar);
+	std::u8string_view const text{u8"Yxmördaren Julia Blomqvist på fäktning i Schweiz"};
+	auto shape_result = segment.text(text).language(fruit::LanguageTag{"sv-se"}).shape(foobar);
 
-		EXPECT_EQ(&shape_result.font(), &face);
+	EXPECT_EQ(&shape_result.font(), &face);
 
-		// Subtract two from std::size(text) because we have ä and å, which occupy two code units in
-		// UTF-8
-		EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 3);
+	// Subtract two from std::size(text) because we have ä and å, which occupy two code units in
+	// UTF-8
+	EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 3);
 
-		auto image = render(shape_result);
-		fruit::io_utils::store(std::as_bytes(make_span(image)),
-							"testdata/Yxmördaren Julia Blomqvist på fäktning i Schweiz.dat");
-	}
+	auto image = render(shape_result);
+	fruit::io_utils::store(std::as_bytes(make_span(image)),
+						"testdata/Yxmördaren Julia Blomqvist på fäktning i Schweiz.dat");
+}
 
-	{
-		std::u8string_view const text{u8"The five boxing wizards jump quickly"};
-		auto shape_result = segment.language(fruit::LanguageTag{"en-gb"}).text(text).shape(foobar);
+TESTCASE(TextSegmentShapeBrittish)
+{
+	fruit::FreetypeFontfaceLoader loader;
+	fruit::FreetypeFontFace face{loader, fruit::io_utils::load(fruit::FontMapper{}.get_path("DejaVu Sans"))};
+	fruit::TextShaper foobar{face};
 
-		// Subtract one for "fi ligature"
-		EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 1);
+	fruit::TextSegment segment;
+	std::u8string_view const text{u8"The five boxing wizards jump quickly"};
+	auto shape_result = segment.language(fruit::LanguageTag{"en-gb"}).text(text).shape(foobar);
 
-		auto image = render(shape_result);
-		fruit::io_utils::store(std::as_bytes(make_span(image)),
-							"testdata/The five boxing wizards jump quickly.dat");
-	}
+	// Subtract one for "fi ligature"
+	EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 1);
 
-	{
-		std::u8string_view const text{u8"Die heiße Zypernsonne quälte Max und Victoria ja böse auf dem Weg bis zur Küste"};
-		auto shape_result = segment.text(text).language(fruit::LanguageTag{"de-de"}).shape(foobar);
+	auto image = render(shape_result);
+	fruit::io_utils::store(std::as_bytes(make_span(image)),
+						"testdata/The five boxing wizards jump quickly.dat");
+}
 
-		EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 4);
+TESTCASE(TextSegmentShapeGerman)
+{
+	fruit::FreetypeFontfaceLoader loader;
+	fruit::FreetypeFontFace face{loader, fruit::io_utils::load(fruit::FontMapper{}.get_path("DejaVu Sans"))};
+	fruit::TextShaper foobar{face};
 
-		auto image = render(shape_result);
-		fruit::io_utils::store(std::as_bytes(make_span(image)),
-							"testdata/Die heiße Zypernsonne quälte Max und Victoria ja böse auf dem Weg bis zur Küste.dat");
-	}
+	fruit::TextSegment segment;
+	std::u8string_view const text{u8"Die heiße Zypernsonne quälte Max und Victoria ja böse auf dem Weg bis zur Küste"};
+	auto shape_result = segment.text(text).language(fruit::LanguageTag{"de-de"}).shape(foobar);
 
-	{
-		std::u8string_view const text{u8"Dès Noël, où un zéphyr haï me vêt de glaçons würmiens, je dîne d’exquis rôtis de bœuf au kir, à l’aÿ d’âge mûr, &cætera"};
-		auto shape_result = segment.text(text).language(fruit::LanguageTag{"fr-fr"}).shape(foobar);
+	EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 4);
 
-		EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 22);
+	auto image = render(shape_result);
+	fruit::io_utils::store(std::as_bytes(make_span(image)),
+						"testdata/Die heiße Zypernsonne quälte Max und Victoria ja böse auf dem Weg bis zur Küste.dat");
+}
 
-		auto image = render(shape_result);
-		fruit::io_utils::store(std::as_bytes(make_span(image)),
-							"testdata/Dès Noël, où un zéphyr haï me vêt de glaçons würmiens, je dîne d’exquis rôtis de bœuf au kir, à l’aÿ d’âge mûr, &cætera.dat");
-	}
+TESTCASE(TextSegmentShapeFrance)
+{
+	fruit::FreetypeFontfaceLoader loader;
+	fruit::FreetypeFontFace face{loader, fruit::io_utils::load(fruit::FontMapper{}.get_path("DejaVu Sans"))};
+	fruit::TextShaper foobar{face};
 
-	{
-		std::u8string_view const text{u8"Taxisjåføren quizet bedre om calypso, watt og klær på hjemveien"};
-		auto shape_result = segment.text(text).language(fruit::LanguageTag{"no"}).shape(foobar);
+	fruit::TextSegment segment;
+	std::u8string_view const text{u8"Dès Noël, où un zéphyr haï me vêt de glaçons würmiens, je dîne d’exquis rôtis de bœuf au kir, à l’aÿ d’âge mûr, &cætera"};
+	auto shape_result = segment.text(text).language(fruit::LanguageTag{"fr-fr"}).shape(foobar);
 
-		EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 4);
+	EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 22);
 
-		auto image = render(shape_result);
-		fruit::io_utils::store(std::as_bytes(make_span(image)),
-							"testdata/Taxisjåføren quizet bedre om calypso, watt og klær på hjemveien.dat");
+	auto image = render(shape_result);
+	fruit::io_utils::store(std::as_bytes(make_span(image)),
+						"testdata/Dès Noël, où un zéphyr haï me vêt de glaçons würmiens, je dîne d’exquis rôtis de bœuf au kir, à l’aÿ d’âge mûr, &cætera.dat");
+}
 
-	}
+TESTCASE(TextSegmentShapeNorwegian)
+{
+	fruit::FreetypeFontfaceLoader loader;
+	fruit::FreetypeFontFace face{loader, fruit::io_utils::load(fruit::FontMapper{}.get_path("DejaVu Sans"))};
+	fruit::TextShaper foobar{face};
 
-	{
-		std::u8string_view const text{u8"Kæmi ný öxi hér, ykist þjólfum nú bæði víl og ádrepa"};
-		auto shape_result = segment.text(text).language(fruit::LanguageTag{"is-is"}).shape(foobar);
+	fruit::TextSegment segment;
+	std::u8string_view const text{u8"Taxisjåføren quizet bedre om calypso, watt og klær på hjemveien"};
+	auto shape_result = segment.text(text).language(fruit::LanguageTag{"no"}).shape(foobar);
 
-		EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 11);
+	EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 4);
 
-		auto image = render(shape_result);
-		fruit::io_utils::store(std::as_bytes(make_span(image)),
-							"testdata/Kæmi ný öxi hér, ykist þjólfum nú bæði víl og ádrepa.dat");
+	auto image = render(shape_result);
+	fruit::io_utils::store(std::as_bytes(make_span(image)),
+						"testdata/Taxisjåføren quizet bedre om calypso, watt og klær på hjemveien.dat");
 
-	}
+}
 
-	{
-		std::u8string_view const text{u8"Vodní žíňky běží kolem lesní tůně a kadeřemi svými čeří stříbrosvit měsíce"};
-		auto shape_result = segment.text(text).language(fruit::LanguageTag{"cs-cz"}).shape(foobar);
+TESTCASE(TextSegmentShapeIcelandic)
+{
+	fruit::FreetypeFontfaceLoader loader;
+	fruit::FreetypeFontFace face{loader, fruit::io_utils::load(fruit::FontMapper{}.get_path("DejaVu Sans"))};
+	fruit::TextShaper foobar{face};
 
-		EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 19);
+	fruit::TextSegment segment;
+	std::u8string_view const text{u8"Kæmi ný öxi hér, ykist þjólfum nú bæði víl og ádrepa"};
+	auto shape_result = segment.text(text).language(fruit::LanguageTag{"is-is"}).shape(foobar);
 
-		auto image = render(shape_result);
-		fruit::io_utils::store(std::as_bytes(make_span(image)),
-							"testdata/Vodní žíňky běží kolem lesní tůně a kadeřemi svými čeří stříbrosvit měsíce.dat");
-	}
+	EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 11);
+
+	auto image = render(shape_result);
+	fruit::io_utils::store(std::as_bytes(make_span(image)),
+						"testdata/Kæmi ný öxi hér, ykist þjólfum nú bæði víl og ádrepa.dat");
+}
+
+TESTCASE(TextSegmentShapeCheck)
+{
+	fruit::FreetypeFontfaceLoader loader;
+	fruit::FreetypeFontFace face{loader, fruit::io_utils::load(fruit::FontMapper{}.get_path("DejaVu Sans"))};
+	fruit::TextShaper foobar{face};
+
+	fruit::TextSegment segment;
+	std::u8string_view const text{u8"Vodní žíňky běží kolem lesní tůně a kadeřemi svými čeří stříbrosvit měsíce"};
+	auto shape_result = segment.text(text).language(fruit::LanguageTag{"cs-cz"}).shape(foobar);
+
+	EXPECT_EQ(std::size(shape_result.glyph_info()), std::size(text) - 19);
+
+	auto image = render(shape_result);
+	fruit::io_utils::store(std::as_bytes(make_span(image)),
+						"testdata/Vodní žíňky běží kolem lesní tůně a kadeřemi svými čeří stříbrosvit měsíce.dat");
 }
