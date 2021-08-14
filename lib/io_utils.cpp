17c4c0d0a0394b3bea56699f4f4fdc36988f97fd
Torbjörn Rathsman <milasudril@gmail.com>
2021-08-14 08:27:08 +0200

    Add generic load function

--- lib/io_utils.cpp
+++ lib/io_utils.cpp
@@ -22,6 +22,25 @@ namespace
 	};
 }
 
+std::vector<std::byte> fruit::io_utils::load(void* source, ReadCallback read_callback, size_t max_num_bytes)
+{
+	std::array<std::byte, 65536> buffer;
+	std::vector<std::byte> ret;
+	ret.reserve(std::min(max_num_bytes, static_cast<size_t>(65536)));
+	while(true)
+	{
+		std::span const read_buffer{std::begin(buffer), std::min(std::size(buffer), std::size(ret) - max_num_bytes)};
+		auto const n_read = read_callback(source, read_buffer);
+		if(n_read == 0)
+		{
+			return ret;
+		}
+		ret.insert(std::end(ret), std::begin(buffer),  std::begin(buffer) + n_read);
+		if(std::size(ret) == max_num_bytes)
+		{ return ret; }
+	}
+}
+
 std::vector<std::byte> fruit::io_utils::load(std::filesystem::path const& path, size_t max_num_bytes)
 {
 	std::unique_ptr<FILE, FileDeleter> file{fopen(path.c_str(), "rb")};
@@ -29,18 +48,10 @@ std::vector<std::byte> fruit::io_utils::load(std::filesystem::path const& path, 
 	{
 		throw std::runtime_error{"Failed to load file"};
 	}
-	std::vector<std::byte> ret;
-	ret.reserve(std::min(max_num_bytes, static_cast<size_t>(65536)));
-	for(size_t k = 0; k < max_num_bytes; ++k)
-	{
-		auto input = getc(file.get());
-		if(input == EOF)
-		{
-			return ret;
-		}
-		ret.push_back(static_cast<std::byte>(input));
-	}
-	return ret;
+
+	return load([f = file.get()](std::span<std::byte> buffer){
+		return fread(std::data(buffer), 1, std::size(buffer), f);
+	}, max_num_bytes);
 }
 
 void fruit::io_utils::store(std::span<std::byte const> data, std::filesystem::path const& path)
--- lib/io_utils.hpp
+++ lib/io_utils.hpp
@@ -14,4 +14,35 @@ namespace fruit::io_utils
 								size_t max_num_bytes = std::numeric_limits<size_t>::max());
 
 	void store(std::span<std::byte const> data, std::filesystem::path const& path);
+
+	using ReadCallback = size_t (*)(void*, std::span<std::byte>);
+
+	std::vector<std::byte> load(void* source, ReadCallback read_callback, size_t max_num_bytes);
+
+	template<class InputStream>
+	requires requires(InputStream s)
+	{
+		{read(s, std::declval<std::span<std::byte>>())} -> std::same_as<size_t>;
+	}
+	std::vector<std::byte> load(InputStream&& source,
+	                            size_t max_num_bytes = std::numeric_limits<size_t>::max())
+	{
+		return load(&source, [](void* handle, std::span<std::byte> buffer){
+			return read(*static_cast<InputStream*>(handle), buffer);
+		}, max_num_bytes);
+	}
+
+	template<class InputStream>
+	requires requires(InputStream s)
+	{
+		{s(std::declval<std::span<std::byte>>())} -> std::same_as<size_t>;
+	}
+	std::vector<std::byte> load(InputStream&& source,
+	                            size_t max_num_bytes = std::numeric_limits<size_t>::max())
+	{
+		return load(&source, [](void* handle, std::span<std::byte> buffer){
+			return (*static_cast<InputStream*>(handle))(buffer);
+		}, max_num_bytes);
+	}
+
 }
