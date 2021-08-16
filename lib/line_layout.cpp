c7290470ce1326796893687fa108f4375f3bd518
Torbjörn Rathsman <milasudril@gmail.com>
2021-08-16 22:12:30 +0200

    Introduce more functionality to LineLayout

--- lib/line_layout.cpp
+++ lib/line_layout.cpp
@@ -3,6 +3,9 @@
 #include "./line_layout.hpp"
 #include "./utils.hpp"
 
+#include <set>
+#include <iterator>
+
 fruit::SizeRequestResult fruit::LineLayout::handle(SizeRequestEvent const&) const
 {
 	auto min_size = std::accumulate(std::begin(m_content), std::end(m_content),
@@ -17,16 +20,100 @@ fruit::SizeRequestResult fruit::LineLayout::handle(SizeRequestEvent const&) cons
 	return SizeRequestResult{min_size, min_size};
 }
 
+struct SizeResult
+{
+	int size;
+};
+
 void fruit::LineLayout::handle(GeometryUpdateEvent const& event)
 {
-	normalize_sum(std::span{std::data(m_content), std::size(m_content)}, [](auto&& item) -> float& {
+	auto content = m_content;
+	normalize_sum(std::span{std::data(content), std::size(content)}, [](auto&& item) -> float& {
 		return item.size;
 	});
 
-	std::ranges::for_each(m_content, [origin = event.location, dir = m_direction](auto item) mutable {
+	if(m_direction == Direction::UpToDown)
+	{
+		auto remaining_boxes = std::size(content);
+		std::vector<ViewportSize> sizes(std::size(content));
+		std::vector<bool> completed(std::size(content));
+		auto size = event.size.height;
+		while(remaining_boxes != 0)
+		{
+			auto const initial_size = size;
+			for(size_t k = 0 ; k != std::size(content); ++k)
+			{
+				if(!completed[k])
+				{
+					auto const& item = content[k];
+					auto const size_req_result = item.event_handler.handle(SizeRequestEvent{});
+					auto const desired_size = static_cast<int>(item.size * initial_size + 0.5f);
+					auto const too_small = desired_size < size_req_result.min_size.height;
+					auto const computed_size = std::max(desired_size, size_req_result.min_size.height);
+					size -= too_small? size_req_result.min_size.height : 0;
+					if(too_small)
+					{
+						sizes[k] = ViewportSize{size_req_result.min_size.width, computed_size};
+						completed[k] = true;
+						content[k].size = 0.0f;
+						--remaining_boxes;
+					}
+				}
+			}
+
+			normalize_sum(std::span{std::data(content), std::size(content)}, [](auto&& item) -> float& {
+				return item.size;
+			});
+
+			if(size == initial_size)
+			{
+				for(size_t k = 0; k != std::size(content); ++k)
+				{
+					if(!completed[k])
+					{
+						auto const& item = content[k];
+						auto const size_req_result = item.event_handler.handle(SizeRequestEvent{});
+						auto const desired_size = static_cast<int>(item.size * initial_size + 0.5f);
+						sizes[k] = ViewportSize{size_req_result.min_size.width, desired_size};
+						completed[k] = true;
+					}
+				}
+				break;
+			}
+		}
+
+		auto origin = event.location;
+		for(size_t k = 0; k != std::size(content); ++k)
+		{
+			content[k].event_handler.handle(GeometryUpdateEvent{sizes[k], origin});
+			origin += Vector{0, sizes[k].height, 0};
+		}
+
+#if 0
+		std::ranges::for_each(m_content, [initial_size = size, &size](auto& item){
+			if(!item.size_was_converted)
+			{
+				auto const size_req_result = item.event_handler.handle(SizeRequestEvent{});
+				auto const desired_size = static_cast<int>(item.size * initial_size);
+				item.size_was_converted = desired_size < size_req_result.min_size.width;
+				item.size_computed = std::max(desired_size, size_req_result.min_size.width);
+				size -= item.size_was_converted ? size_req_result.min_size.width : 0;
+			}
+		});
+#endif
+	}
+
+
+#if 0
+	std::ranges::for_each(m_content, [origin = event.location,
+						  input_size = m_direction == Direction::LeftToRight?
+							event.size.width:event.size.height,
+						  dir = m_direction](auto const& item) mutable {
 		auto const res = item.event_handler.handle(SizeRequestEvent{});
-		auto const size = res.min_size;
+		auto const size = std::max(dir == Direction::LeftToRight? res.min_size.width : res.min_size.height,
+								   static_cast<int>(item.size * input_size));
 		item.event_handler.handle(GeometryUpdateEvent{size, origin});
-		origin += dir == Direction::LeftToRight? Vector{size.width, 0, 0} : Vector{0, size.height, 0};
+		origin += dir == Direction::LeftToRight? Vector{size, 0, 0} : Vector{0, size, 0};
 	});
+#endif
 }
--- lib/line_layout.test.cpp
+++ lib/line_layout.test.cpp
@@ -70,3 +70,63 @@ TESTCASE(LineLayoutGeometryUpdate)
 	EXPECT_EQ(b.size, size_b);
 	EXPECT_EQ(c.size, size_c);
 }
+
+TESTCASE(LineLayoutGeometryUpdateNoConstraints)
+{
+	fruit::LineLayout layout{fruit::LineLayout::Direction::UpToDown};
+
+	fruit::ViewportSize size_a{2, 0};
+	fruit::ViewportSize size_b{3, 0};
+	fruit::ViewportSize size_c{1, 0};
+
+	Object a{size_a, fruit::Origin<int>};
+	Object b{size_b, fruit::Origin<int>};
+	Object c{size_c, fruit::Origin<int>};
+
+	layout.push_back(fruit::LayoutBox{std::ref(a), 1.0f});
+	layout.push_back(fruit::LayoutBox{std::ref(b), 1.0f});
+	layout.push_back(fruit::LayoutBox{std::ref(c), 1.0f});
+
+	layout.handle(fruit::GeometryUpdateEvent{fruit::ViewportSize{3, 3}, fruit::Point{3, 4, 5}});
+	EXPECT_EQ(a.location.x(), 3);
+	EXPECT_EQ(b.location.x(), 3);
+	EXPECT_EQ(c.location.x(), 3);
+
+	EXPECT_EQ(a.location.y(), 4);
+	EXPECT_EQ(b.location.y(), 5);
+	EXPECT_EQ(c.location.y(), 6);
+
+	EXPECT_EQ(a.size, (fruit::ViewportSize{size_a.width, 1}));
+	EXPECT_EQ(b.size, (fruit::ViewportSize{size_b.width, 1}));
+	EXPECT_EQ(c.size, (fruit::ViewportSize{size_c.width, 1}));
+}
+
+TESTCASE(LineLayoutGeometryUpdatePartialConstraints)
+{
+	fruit::LineLayout layout{fruit::LineLayout::Direction::UpToDown};
+
+	fruit::ViewportSize size_a{2, 0};
+	fruit::ViewportSize size_b{3, 2};
+	fruit::ViewportSize size_c{1, 0};
+
+	Object a{size_a, fruit::Origin<int>};
+	Object b{size_b, fruit::Origin<int>};
+	Object c{size_c, fruit::Origin<int>};
+
+	layout.push_back(fruit::LayoutBox{std::ref(a), 1.0f});
+	layout.push_back(fruit::LayoutBox{std::ref(b), 1.0f});
+	layout.push_back(fruit::LayoutBox{std::ref(c), 1.0f});
+
+	layout.handle(fruit::GeometryUpdateEvent{fruit::ViewportSize{3, 4}, fruit::Point{3, 4, 5}});
+	EXPECT_EQ(a.location.x(), 3);
+	EXPECT_EQ(b.location.x(), 3);
+	EXPECT_EQ(c.location.x(), 3);
+
+	EXPECT_EQ(a.location.y(), 4);
+	EXPECT_EQ(b.location.y(), 5);
+	EXPECT_EQ(c.location.y(), 7);
+
+	EXPECT_EQ(a.size, (fruit::ViewportSize{size_a.width, 1}));
+	EXPECT_EQ(b.size, (fruit::ViewportSize{size_b.width, 2}));
+	EXPECT_EQ(c.size, (fruit::ViewportSize{size_c.width, 1}));
+}
