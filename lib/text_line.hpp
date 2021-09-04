fe8a1ecb48fa16e266a77fa26d9fa6ac0f928ac4
Torbjörn Rathsman <milasudril@gmail.com>
2021-09-04 10:32:28 +0200

    Hide implementation details for TextLine
    Will be useful when TextElement (multiline) is added

--- lib/text_line.hpp
+++ lib/text_line.hpp
@@ -8,11 +8,91 @@
 
 namespace fruit
 {
-	struct TextLine
+	class TextLine
 	{
-		TextSegment text;
-		int size;
-		std::reference_wrapper<FontFace> font_face;
+	public:
+		TextLine(std::reference_wrapper<FontFace const> font):
+			m_text{},
+			m_size{16},
+			m_font{font}
+		{}
+
+		TextLine& text(std::basic_string_view<char8_t> buffer) &
+		{
+			m_text.text(buffer);
+			return *this;
+		}
+
+		TextLine&& text(std::basic_string_view<char8_t> buffer) &&
+		{
+			m_text.text(buffer);
+			return std::move(*this);
+		}
+
+		TextLine& direction(TextDirection val) &
+		{
+			m_text.direction(val);
+			return *this;
+		}
+
+		TextLine&& direction(TextDirection val) &&
+		{
+			m_text.direction(val);
+			return std::move(*this);
+		}
+
+		TextLine& language(LanguageTag const& lang) &
+		{
+			m_text.language(lang);
+			return *this;
+		}
+
+		TextLine&& language(LanguageTag const& lang) &&
+		{
+			m_text.language(lang);
+			return std::move(*this);
+		}
+
+		TextLine& script(WritingSystem val) &
+		{
+			m_text.script(val);
+			return *this;
+		}
+
+		TextLine&& script(WritingSystem val) &&
+		{
+			m_text.script(val);
+			return std::move(*this);
+		}
+
+		TextLine& size(int size) &
+		{
+			m_size = size;
+			return *this;
+		}
+
+		TextLine&& size(int size) &&
+		{
+			m_size = size;
+			return std::move(*this);
+		}
+
+		TextLine& font(FontFace& font) &
+		{
+			m_font = font;
+			return *this;
+		}
+
+		TextLine&& font(FontFace& font) &&
+		{
+			m_font = font;
+			return std::move(*this);
+		}
+
+	private:
+		TextSegment m_text;
+		int m_size;
+		std::reference_wrapper<FontFace const> m_font;
 	};
 }
 
--- lib/text_line.test.cpp
+++ lib/text_line.test.cpp
@@ -7,13 +7,5 @@
 TESTCASE(TextLineCreateObj)
 {
 	fruit::FontFace face;
-	fruit::TextLine obj{
-		fruit::TextSegment{}.text(u8"Hello, World"),
-		16,
-		face
-	};
-
-	EXPECT_EQ(obj.size, 16);
-	EXPECT_EQ(obj.text.valid(), true);
-	EXPECT_EQ(obj.font_face.get().valid(), false);
+	fruit::TextLine obj{face};
 }
