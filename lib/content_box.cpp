26103e3de347f9cf31fa572188c0de634dd41111
Torbjörn Rathsman <milasudril@gmail.com>
2021-09-05 11:29:08 +0200

    Move implementation of handle size req event

--- lib/content_box.cpp
+++ lib/content_box.cpp
@@ -1,3 +1,20 @@
 //@	{"target":{"name":"content_box.o"}}
 
 #include "./content_box.hpp"
+
+fruit::SizeRequestResult fruit::ContentBox::handle(SizeRequestEvent const& event) const
+{
+	auto size = m_padding_near
+		+ m_padding_far
+		+ m_margin_near
+		+ m_margin_far
+		+ m_border_width_near
+		+ m_border_width_far;
+
+	auto res = m_content.handle(event).min_size;
+	size += Vector{res.width, res.height, 0};
+
+	res = ViewportSize{size.x(), size.y()};
+
+	return SizeRequestResult{res, res};
+}
--- lib/content_box.hpp
+++ lib/content_box.hpp
@@ -13,22 +13,7 @@ namespace fruit
 	class ContentBox
 	{
 	public:
-		SizeRequestResult handle(SizeRequestEvent const& event) const
-		{
-			auto size = m_padding_near
-				+ m_padding_far
-				+ m_margin_near
-				+ m_margin_far
-				+ m_border_width_near
-				+ m_border_width_far;
-
-			auto res = m_content.handle(event).min_size;
-			size += Vector{res.width, res.height, 0};
-
-			res = ViewportSize{size.x(), size.y()};
-
-			return SizeRequestResult{res, res};
-		}
+		SizeRequestResult handle(SizeRequestEvent const& event) const;
 
 		void handle(GeometryUpdateEvent const& event);
 
