74a6d07780809cb00382194a19a59f9e1732c96d
Torbjörn Rathsman <milasudril@gmail.com>
2021-09-05 14:26:26 +0200

    Drafting UpdateEventSw for ContentBox

--- lib/content_box.cpp
+++ lib/content_box.cpp
@@ -18,3 +18,45 @@ fruit::SizeRequestResult fruit::ContentBox::handle(SizeRequestEvent const& event
 
 	return SizeRequestResult{res, res};
 }
+
+void fruit::ContentBox::handle(UpdateEventSw const& event) const
+{
+	auto const buffer = event.buffer;
+	{
+		// render background
+		auto const rect_begin = m_location;
+		auto const rect_end = m_location + Vector{m_width, m_height, 0};
+		fill_rect(buffer, rect_begin, rect_end, m_bg_color);
+	}
+
+	m_content.compose(buffer, m_text_color);
+
+	{
+		// render top border
+		auto const rect_begin = m_location;
+		auto const rect_end = m_location + Vector{m_width, m_border_width_near.y(), 0};
+		fill_rect(buffer, rect_begin, rect_end, m_border_color);
+	}
+
+	{
+		// render bottom border
+		auto const rect_begin = m_location + Vector{0, -m_border_width_near.y(), 0};
+		auto const rect_end = m_location + Vector{m_width, m_height, 0};
+		fill_rect(buffer, rect_begin, rect_end, m_border_color);
+	}
+
+	{
+		// render left border
+		auto const rect_begin = m_location
+		auto const rect_end = m_location + Vector{m_border_width_near.x(), m_height, 0};
+		fill_rect(buffer, rect_begin, rect_end, m_border_color);
+	}
+
+	{
+		// render right border
+		auto const rect_begin = m_location  + Vector{-m_border_width_near.x(), 0, 0};
+		auto const rect_end = m_location + Vector{m_width, m_height, 0};
+		fill_rect(buffer, rect_begin, rect_end, m_border_color);
+	}
+
+}
