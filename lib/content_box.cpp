6a6c2e2602917add15e14c9018fe3e7acdc70e46
Torbjörn Rathsman <milasudril@gmail.com>
2021-09-05 11:21:23 +0200

    Inintial work on ContentBox implementation

---
+++ lib/content_box.cpp
@@ -0,0 +1,3 @@
+//@	{"target":{"name":"content_box.o"}}
+
+#include "./content_box.hpp"
--- lib/content_box.hpp
+++ lib/content_box.hpp
@@ -1,38 +1,56 @@
+//@	{"dependencies_extra":[{"ref":"./content_box.o", "rel":"implementation"}]}
+
 #ifndef FRUIT_CONTENTBOX_HPP
 #define FRUIT_CONTENTBOX_HPP
 
 #include "./text_line.hpp"
+#include "./size_request_event.hpp"
+#include "./geometry_update_event.hpp"
+#include "./update_event.hpp"
 
 namespace fruit
 {
-	struct ContentBox
+	class ContentBox
 	{
-		Point<int> locaction;
-		int current_width;
-		int current_height;
+	public:
+		SizeRequestResult handle(SizeRequestEvent const& event) const
+		{
+			auto size = m_padding_near
+				+ m_padding_far
+				+ m_margin_near
+				+ m_margin_far
+				+ m_border_width_near
+				+ m_border_width_far;
 
-		int padding_left;
-		int padding_right;
-		int padding_top;
-		int padding_bottom;
+			auto res = m_content.handle(event).min_size;
+			size += Vector{res.width, res.height, 0};
 
-		int margin_left;
-		int margin_right;
-		int margin_top;
-		int margin_bottom;
+			res = ViewportSize{size.x(), size.y()};
 
-		Pixel bg_color;
-		Pixel border_color;
-		int border_width;
-
-		TextLine content;
-		Pixel text_color;
-
-		SizeRequestResult handle(SizeRequestEvent const&) const;
+			return SizeRequestResult{res, res};
+		}
 
 		void handle(GeometryUpdateEvent const& event);
 
 		void handle(UpdateEventSw const& event) const;
+
+	private:
+		Point<int> m_loc;
+		Vector<int> m_padding_near;
+		Vector<int> m_padding_far;
+		Vector<int> m_margin_near;
+		Vector<int> m_margin_far;
+		Vector<int> m_border_width_near;
+		Vector<int> m_border_width_far;
+
+		TextLine m_content;
+		Pixel m_text_color;
+
+		int m_current_width;
+		int m_current_height;
+
+		Pixel m_bg_color;
+		Pixel m_border_color;
 	};
 }
 
